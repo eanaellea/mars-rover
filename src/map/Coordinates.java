c66fda20400304d474b49ff2c5df34888ba14fee
aea <aea@myges.fr>
2023-11-22 00:16:22 +0100

    create Coordinates class

---
+++ src/map/Coordinates.java
@@ -0,0 +1,62 @@
+package map;
+
+import java.util.Objects;
+
+public class Coordinates {
+    private int x;
+    private int y;
+
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void incrementX() {
+        this.x++;
+    }
+
+    public void incrementY() {
+        this.y++;
+    }
+
+    public void decrementX() {
+        this.x--;
+    }
+
+    public void decrementY() {
+        this.y--;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinates that = (Coordinates) o;
+        return x == that.x && y == that.y;
+    }
+
+    @Override
+    public String toString() {
+        return "Coordinates{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
+
